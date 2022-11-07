8d2d833b25a2f43ef7f75fa22691516852ba99b0
ArikoVi <ar.iko.vi253@yandex.ru>
2022-11-07 21:33:19 +0400

    обращайся))

--- components/title_text.js
+++ components/title_text.js
@@ -5,7 +5,7 @@ export default function TitleText() {
         <div className="max-w-5xl flex flex-row justify-end">
             <div className="">
                 <Sphere className="static"></Sphere>
-            <h1 className="text-6xl text-left font-light content_animate z-10">Designer & art director creating scalable design solutions in branding, packaging and web design, striving for clarity, simplicity & logic.</h1>
+            <h1 className="text-6xl text-left font-light content_animate z-10 relative">Designer & art director creating scalable design solutions in branding, packaging and web design, striving for clarity, simplicity & logic.</h1>
 
             </div>
         </div>
