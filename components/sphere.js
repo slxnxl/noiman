3aa57c5f79687b5e0aedf6f8b49aead35d7d7ca7
Sypkov <sypkovstepan@gmail.com>
2022-11-14 12:11:22 +0500

    add new 3d sphere with animate

--- components/sphere.js
+++ components/sphere.js
@@ -1,28 +1,49 @@
 import React from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import {Canvas, useFrame} from "@react-three/fiber";
+import {
+    MeshDistortMaterial,
+    MeshWobbleMaterial,
+    Sphere
+} from "@react-three/drei";
 
 function MyRotatingBox() {
-    const sphere = React.useRef();
-
-    useFrame(({ clock }) => {
-        const a = clock.getElapsedTime();
-        sphere.current.rotation.x = a;
-    });
+    // const sphere = React.useRef();
+    // let p = sphere.vertices;
+    // console.log(sphere);
+    // useFrame(({ clock }) => {
+    //     const time = performance.now() * 0.0017;
+    //     sphere.current.rotation.x = clock.getElapsedTime();
+    //     const k = 1;
+    //     for (let i = 0; i < p.length(); i++) {
+    //         p.normalize().multiplyScalar(1 + 0.3 * noise.perlin3(p.x * k + time, p.y * k, p.z * k));
+    //     }
+    //         sphere.geometry.computeVertexNormals();
+    //         sphere.geometry.normalsNeedUpdate = true;
+    //         sphere.geometry.verticesNeedUpdate = true;
+    // });
 
     return (
-        <mesh ref={sphere}>
-            <sphereGeometry args={[2, 128, 128]} />
-            <meshNormalMaterial></meshNormalMaterial>
-        </mesh>
+            <Sphere visible position={[3, 0, 0]} args={[1, 16, 200]}>
+                <MeshDistortMaterial
+                    color="#00A38"
+                    attach="material"
+                    distort={0.3} // Strength, 0 disables the effect (default=1)
+                    speed={2} // Speed (default=1)
+                    roughness={0}
+                />
+            {/*<sphereGeometry args={[2, 128, 128]} />*/}
+            {/*<boxGeometry attach="geometry" args={[1, 1, 1]} />*/}
+            {/*<meshNormalMaterial></meshNormalMaterial>*/}
+            </Sphere>
     );
 }
 
-export default function Sphere() {
+export default function Sphere1() {
     return (
         <div  className="absolute inset-0 z-0 content_animate">
             <Canvas>
                 <MyRotatingBox />
-                <ambientLight intensity={0.1} />
+                <ambientLight intensity={0.3} />
                 <directionalLight />
             </Canvas>
         </div>
--- components/title_text.js
+++ components/title_text.js
@@ -1,10 +1,10 @@
-import Sphere from "./sphere";
+import Sphere1 from "./sphere";
 
 export default function TitleText() {
     return(
         <div className="max-w-5xl flex flex-row justify-end">
             <div className="">
-                <Sphere className="static"></Sphere>
+                <Sphere1 className="static"></Sphere1>
             <h1 className="text-6xl text-left font-light content_animate z-10 relative">Designer & art director creating scalable design solutions in branding, packaging and web design, striving for clarity, simplicity & logic.</h1>
             </div>
         </div>
