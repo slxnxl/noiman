916691f53e42fce4ef9f23c1e7ed279604873a1a
Sypkov <sypkovstepan@gmail.com>
2022-11-23 14:28:45 +0500

    remove shadow in sphere

--- components/new_sphere.js
+++ components/new_sphere.js
@@ -70,11 +70,11 @@ export default function Scene({ setBg }) {
                         setMode(!mode)
                         // setBg({ background: !mode ? '#0d1010' : '#f0f0f0', fill: !mode ? '#f0f0f0' : '#202020' })
                     }}>
-                    <sphereBufferGeometry args={[1, 192, 192]} />
+                    <sphereBufferGeometry args={[0.9, 192, 192]} />
                     <AnimatedMaterial color={color} envMapIntensity={env} clearcoat={coat} clearcoatRoughness={0} metalness={0.1} />
                 </a.mesh>
                 <Environment preset="warehouse" />
-                <ContactShadows rotation={[Math.PI / 2, 0, 0]} position={[0, -1.6, 0]} opacity={0.4} width={15} height={15} blur={2.5} far={1.6} />
+                {/*<ContactShadows rotation={[Math.PI / 2, 0, 0]} position={[0, -1.6, 0]} opacity={0.4} width={15} height={15} blur={2.5} far={1.6} />*/}
             </Suspense>
         </>
     )
