96838cdcacaaa67242d822893afb7e65b057fa53
Sypkov <sypkovstepan@gmail.com>
2022-11-23 15:06:20 +0500

    chage index

--- components/title_text_grid.js
+++ components/title_text_grid.js
@@ -7,11 +7,11 @@ export default function TitleTextGrid() {
         <div className="grid lg:grid-cols-4 gap-4 font-normal text-xl gap-8 lg:gap-x-5 lg:gap-y-16 sm:gap-y-6 grid-flow-dense sm:grid-cols-1 text-lg" >
             <div className="col-start-2 col-span-4">
                 {/*<Sphere1></Sphere1>*/}
-                <div className=" absolute inset-0 z-0">
+                <div className=" z-0">
                     <Canvas className="canvas " pixelRatio={[1, 2]}>
                         <Scene></Scene>
                     </Canvas></div>
-                <h1 className="text-6xl text-left font-light content_animate z-10 relative">Designer & art director creating scalable design solutions in branding, packaging and web design, striving for clarity, simplicity & logic.</h1>
+                <h1 className="text-6xl text-left font-light content_animate z-10">Designer & art director creating scalable design solutions in branding, packaging and web design, striving for clarity, simplicity & logic.</h1>
             </div>
         </div>
         // <div className="max-w-5xl flex flex-row justify-end">
