70809a0f766d68211575aa34a74d611d63fa8f08
ArikoVi <ar.iko.vi253@yandex.ru>
2022-11-23 15:07:44 +0400

    sphere grig

--- components/title_text_grid.js
+++ components/title_text_grid.js
@@ -5,12 +5,13 @@ import { Canvas } from "@react-three/fiber";
 export default function TitleTextGrid() {
     return (
         <div className="grid lg:grid-cols-4 gap-4 font-normal text-xl gap-8 lg:gap-x-5 lg:gap-y-16 sm:gap-y-6 grid-flow-dense sm:grid-cols-1 text-lg" >
+            <div className="inset-0 z-0">
+                    <Canvas className="canvas " pixelRatio={[1, 2]}>
+                        <Scene/>
+                    </Canvas>
+                </div>
             <div className="col-start-2 col-span-4">
                 {/*<Sphere1></Sphere1>*/}
-                <div className=" absolute inset-0 z-0">
-                    <Canvas className="canvas " pixelRatio={[1, 2]}>
-                        <Scene></Scene>
-                    </Canvas></div>
                 <h1 className="text-6xl text-left font-light content_animate z-10 relative">Designer & art director creating scalable design solutions in branding, packaging and web design, striving for clarity, simplicity & logic.</h1>
             </div>
         </div>
