9a27c4260ca3c7ff7274f1bbc5aeb2090cc31dc7
Sypkov <sypkovstepan@gmail.com>
2022-12-27 20:30:41 +0500

    drop scroll-smooth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
 import "../styles/globals.css";
+// import React, { useEffect, useRef } from "react";
+// import { Scrollbar } from "smooth-scrollbar-react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // const scrollbar = useRef();
+  //
+  // useEffect(() => {
+  //   console.log("scrollbar: ", scrollbar.current);
+  // }, []);
+
+  return (
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     position: "fixed",
+    //     height: "100vh",
+    //     // width: "100vw",
+    //   }}
+    // >
+    //     test smooth-scroll
+    //   <Scrollbar
+    //     // style={{ margin-led: "auto" }}
+    //     damping={0.05}
+    //     thumbMinSize={40}
+    //     renderByPixels={true}
+    //     continuousScrolling={false}
+    //     ref={scrollbar}
+    //     plugins={{
+    //       overscroll: {
+    //         effect: "bounce",
+    //       },
+    //     }}
+    //   >
+    <Component {...pageProps} />
+    //   </Scrollbar>
+    // </div>
+  );
 }
 
 export default MyApp;
